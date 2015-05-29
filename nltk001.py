d4f31e79ec1e461aab1e84766c7d6212db5f3603
Aaron Peters <transreductionist@gmail.com>
2015-05-29 08:06:24 -0400

    NLTK workbench
    Pure Python project for working in NLTK

---
+++ nltk001.py
@@ -0,0 +1,26 @@
+# from nltk.corpus import reuters
+# from nltk.corpus import brown
+# import nltk
+from nltk.tokenize import RegexpTokenizer
+import os
+from nltk.corpus import PlaintextCorpusReader
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__)).replace('\\', '/')
+
+corpus_root = BASE_DIR + '/NLTK/TestCorpus/'
+
+my_corpus = PlaintextCorpusReader(corpus_root,
+                    ['testa1.txt', 'testa2.txt', 'testa3.txt', 'testa4.txt', 'testa5.txt', 'testa6.txt'],
+                    encoding='latin-1')
+
+print(my_corpus.fileids())
+
+tokenizer = RegexpTokenizer(r'\w+')
+raw = my_corpus.raw().lower()
+
+
+stop = 1
+
+
+
+
